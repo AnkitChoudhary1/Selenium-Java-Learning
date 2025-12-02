7852ecd262707377e294ff050597d534cace808f
Ankit Choudhary1 <ankit.choudhary2792@gmail.com>
2025-12-02 08:51:11 +0530

    Second script for learning

---
+++ src/test/java/Cucubmer/DropdownPractice.java
@@ -0,0 +1,36 @@
+package Cucubmer;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class DropdownPractice {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        driver.manage().window().maximize();
+
+
+        //Login code
+        driver.findElement(By.cssSelector("input[name='user-name']")).sendKeys("standard_user");
+        driver.findElement(By.xpath("//*[@name='password']")).sendKeys("secret_sauce");
+        driver.findElement(By.xpath("//*[@type='submit']")).click();
+
+        //Need to store the xpath of the the drop down into web element to pass it as parameter in select class
+        WebElement dropDown = driver.findElement(By.xpath("//*[@data-test='product-sort-container']"));
+
+        //Not writing the select class
+        Select filter = new Select(dropDown);
+
+        //selecting the option to get dropdown
+        filter.selectByVisibleText("Price (low to high)");
+
+        System.out.println("✅ Dropdown selected successfully!");
+
+        Thread.sleep(6000);
+        driver.quit();
+
+    }
+}
