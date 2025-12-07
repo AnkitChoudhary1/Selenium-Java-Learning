606d4279652232c90894618485af55f1c0453e6e
Ankit Choudhary1 <ankit.choudhary2792@gmail.com>
2025-12-07 09:14:35 +0530

    Added auto suggestion code

--- src/test/java/Cucubmer/AutoSuggest.java
+++ src/test/java/Cucubmer/AutoSuggest.java
@@ -2,7 +2,10 @@ package Cucubmer;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class AutoSuggest {
     public static void main(String[] arg) throws InterruptedException {
@@ -12,7 +15,22 @@ public class AutoSuggest {
        // driver.findElement(By.xpath("//*[@placeholder='Type to Select']")).click();
         driver.findElement(By.xpath("//*[@placeholder='Type to Select']")).sendKeys("ind");
 
-        Thread.sleep(2000);
+        Thread.sleep(3000);
+        // 3. Grab all options using findElements (Plural!)
+        // The CSS selector usually targets the generic list items 'li' or 'a' tags
+        List<WebElement> options = driver.findElements(By.xpath("//*[@class='ui-menu-item']"));
+
+        for(WebElement option : options){
+            if(option.getText().equalsIgnoreCase("india")){
+                option.click();
+            }
+        }
+        System.out.println("The click was a success ");
+        String value = driver.findElement(By.xpath("//*[@id='autosuggest']")).getDomProperty("value");
+        System.out.println("The value is = "+value);
+
+
+
 
     }
 }
