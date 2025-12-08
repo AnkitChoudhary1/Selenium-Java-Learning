e2f157098596d33ef392d01579ac69eb9d9b6e90
shanmukh0028 <ankit.choudhary2792@gmail.com>
2025-12-08 12:47:28 +0530

    Added pop up

--- src/test/java/Cucubmer/AlertTest.java
+++ src/test/java/Cucubmer/AlertTest.java
@@ -12,5 +12,20 @@ public class AlertTest {
         String name ="Ankit";
         driver.findElement(By.xpath("//*[@placeholder='Enter Your Name']")).sendKeys("Ankit");
         driver.findElement(By.xpath("//*[@value='Alert']")).click();
+
+        // 2. Switch context to the Alert
+        // Note: We are not creating a WebElement here, we are switching the driver focus
+        System.out.println(driver.switchTo().alert().getText());
+
+        //Click on Ok accept
+        driver.switchTo().alert().accept();
+
+        // 4. Test the Confirm button (which has OK and Cancel)
+        driver.findElement(By.xpath("//*[@onclick='displayConfirm()']")).click();
+        System.out.println(driver.switchTo().alert().getText());
+        //click on dismiss button
+        driver.switchTo().alert().dismiss();
+
+        driver.quit();
     }
 }
