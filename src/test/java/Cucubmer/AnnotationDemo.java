e71e863b1936d7f24f4904e7d571ea7cce73ed34
Ankit Choudhary1 <ankit.choudhary2792@gmail.com>
2025-12-10 19:31:25 +0530

    Added test ng 2

---
+++ src/test/java/Cucubmer/AnnotationDemo.java
@@ -0,0 +1,44 @@
+package Cucubmer;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class AnnotationDemo {
+
+    //Declaring webDriver globally to make sure
+    WebDriver driver;
+
+    @BeforeMethod
+    public void openBrowser(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(6));
+    }
+
+    @Test
+    public void googleTest(){
+        driver.get("https://www.google.com/");
+        System.out.println(driver.getTitle());
+    }
+
+    @Test
+    public void amazonTest(){
+        driver.get("https://www.amazon.com/");
+        System.out.println(driver.getTitle());
+    }
+
+
+    @AfterMethod
+    public void closeBrowser(){
+        System.out.println("Closing the browser");
+        driver.quit();
+    }
+
+
+
+}
