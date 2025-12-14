ebbe7e823f451e5cadda25ef16ba9467a3abf1da
Ankit Choudhary1 <ankit.choudhary2792@gmail.com>
2025-12-14 20:04:49 +0530

    Added the testng run and always run

--- src/test/java/Cucubmer/AnnotationDemo.java
+++ src/test/java/Cucubmer/AnnotationDemo.java
@@ -13,27 +13,27 @@ public class AnnotationDemo {
     //Declaring webDriver globally to make sure
     WebDriver driver;
 
-    @BeforeMethod
+    @BeforeMethod(alwaysRun =true)
     public void openBrowser(){
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(6));
     }
 
-    @Test(priority =1)
+    @Test(priority =1, groups ="Smoke")
     public void googleTest(){
         driver.get("https://www.google.com/");
         System.out.println(driver.getTitle());
     }
 
-    @Test(priority =2)
+    @Test(priority =2, groups ="Regression")
     public void amazonTest(){
         driver.get("https://www.amazon.com/");
         System.out.println(driver.getTitle());
     }
 
 
-    @AfterMethod
+    @AfterMethod(alwaysRun = true)
     public void closeBrowser(){
         System.out.println("Closing the browser");
         driver.quit();
---
+++ src/test/java/Cucubmer/DataProviderDemo.java
@@ -0,0 +1,4 @@
+package Cucubmer;
+
+public class DataProviderDemo {
+}
